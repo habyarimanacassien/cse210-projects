1e72dde7d2609ff5d9aeac08e7c8f26ec857c8e5
habyarimanacassien <c.habyarimana@ur.ac.rw>
2025-03-08 20:19:29 +0200

    Write code for guessing game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string needMore = "yes";
+        while (needMore == "yes")
+        {
+            Random randGen = new Random();
+            int magic = randGen.Next(1, 120);
+
+            string keep = "yes";
+            int guesses = 0;
+            while (keep == "yes")
+            {
+                Console.Write("What is your guess? ");
+                string userInput2 = Console.ReadLine();
+                int guess = int.Parse(userInput2);
+
+                if(guess > magic)
+                {
+                    Console.WriteLine("Lower");
+                    keep = "yes";
+                }
+                else if (guess < magic)
+                {
+                    Console.WriteLine("Higher");
+                    keep = "yes";
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    keep = "no";
+                }
+                guesses += 1;
+            }
+            Console.WriteLine($"The number of total guesses was {guesses}.");
+
+            Console.Write("Do you want to play again? ");
+            needMore = Console.ReadLine();
+        }
+        Console.WriteLine("Good bye!!!");
     }
 }
