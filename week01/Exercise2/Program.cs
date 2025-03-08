3cdffe1c3b89b70773b2000cc3956a1fa7e8df25
habyarimanacassien <c.habyarimana@ur.ac.rw>
2025-03-08 19:04:00 +0200

    Write code to show grade letter to the use

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,62 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your grade percentage (eg. type 71 if it is 71%): ");
+        string userGrade = Console.ReadLine();
+        int grade = int.Parse(userGrade);
+
+        string letter;
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        string sign;
+        int mod = grade % 10;
+        Console.WriteLine($"Your sign is {mod}");
+        if (mod >= 7 && letter != "A" && letter != "F")
+        {
+            sign = "+";
+        }
+        else if (mod < 3 && letter != "F")
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}{sign}");
+
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you failed the course! Don't give up, you will do it next time.");
+        }
     }
 }
