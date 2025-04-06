11554ec10cd00b8912ae84a43103ac28d1ced800
habyarimanacassien <c.habyarimana@ur.ac.rw>
2025-04-06 15:59:09 +0200

    Classes Assignment.cs, MathAssignment.cs, and WritingAssignment.cs created and code written.

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Test the base assignment class
+        Assignment assignment = new Assignment("Cassien Habyarimana", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine();
+
+        // Test the MathAssignment class
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rogriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine();
+
+        // Test the WritingAssignment class
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
